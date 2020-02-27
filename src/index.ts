6d1b2205486ce5635cd756fab6dba66a18465e15
Jeroen Claassens <j.claassens@cgi.com>
2020-02-27 11:59:13 +0100

    add output to index file

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,11 @@ const greeter = new HelloCGI();
 const greeting = greeter.greet('John Connor');
 
 console.log(greeting);
+
+// [Method Start (greet)]: with arguments John Connor
+// [Result (greet)]: => Welcome to CGI, John Connor!
+// Welcome to CGI, John Connor!
+
+
+
+
--- src/program.ts
+++ src/program.ts
@@ -15,3 +15,6 @@ export class HelloCGI implements HelloCGIOptions {
     }
   }
 }
+
+
+
